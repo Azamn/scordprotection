da9fa48067b1770b1cd4aad1716c5f41bfc357c1
cshoaib05 <choudharyshoaib05@gmail.com>
2021-09-06 01:15:14 +0530

    Added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,82 @@
+<footer id="footer">
+    <div class="footer-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="widget">
+
+                        <div class="widget-title">About Us</div>
+                        <p class="mb-5">Built with love in Fort Worth, Texas, USA<br> All rights reserved.
+                            Copyright © 2021. INSPIRO. The most happiest time of the day!. Praesent id dolor
+                            dui, dapibus gravida elit. Donec consequat laoreet sagittis. Suspendisse ultricies
+                            ultrices viverra. Morbi rhoncus laoreet tincidunt. Mauris interdum convallis metus.
+                            Suspendisse vel lacus est, sit amet tincidunt erat. Etiam purus sem, euismod eu
+                            vulputate eget, porta quis sapien. Donec tellus est, rhoncus vel scelerisque id,
+                            iaculis eu nibh</p>
+                        <!-- <a href="https://themeforest.net/item/polo-responsive-multipurpose-html5-template/13708923" class="btn btn-inverted" target="_blank">Purchase Now</a> -->
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Menu</div>
+                                <ul class="list">
+                                    <li><a href="scord.html">Home</a></li>
+                                    <li><a href="scordaboutus.html">About Us</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="scordcontactus.html">Contact Us</a></li>
+                                    <li><a href="#">Bouncers</a></li>
+                                    <!-- <li><a href="#">Customers</a></li> -->
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Our Services</div>
+                                <ul class="list">
+                                    <li><a href="#">Armed and Unarmed Forces</a></li>
+                                    <li><a href="#">Security Officers</a></li>
+                                    <li><a href="#">Bouncers</a></li>
+                                    <li><a href="#">Lady Searchers</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Pages</div>
+                                <ul class="list">
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Contact Us</div>
+                                <ul class="list">
+                                    <li><a href="#">Address : labcdefghijklmnop q keef DSF dsfdsf dsfds vsd</a>
+                                    </li>
+                                    <li><a href="#">Call Us : +91-9876543210</a></li>
+                                    <li><a href="#">Mail Us : qwerty@gmail.com</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <div class="copyright-content">
+        <div class="container">
+            <div class="copyright-text text-center">&copy; 2021 POLO - Responsive Multi-Purpose HTML5 Template.
+                All Rights Reserved.<a href="https://www.inspiro-media.com" target="_blank" rel="noopener">
+                    INSPIRO</a> </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,49 @@
+<header id="header" data-transparent="true" class="submenu-light ">
+    <div class="header-inner">
+        <div class="container">
+            <!--Logo-->
+            <div id="logo">
+                <a href="scord.html">
+                    <img src="images/scordimg/Scord.png" class="pb-2" alt="">
+                </a>
+            </div>
+
+            <div id="mainMenu-trigger"> <a class="lines-button x"><span class="lines"></span></a> </div>
+            <!--end: Navigation Resposnive Trigger-->
+            <!--Navigation-->
+            <div id="mainMenu">
+                <div class="container">
+                    <nav>
+                        <ul>
+                            <li><a href="scord.html">Home</a></li>
+                            <li class="dropdown"><a href="scordaboutus.html">About Us</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="scordaboutus.html">About Us</a></li>
+                                    <li><a href="#">Quality Policy</a></li>
+                                    <li><a href="#">SCORD Facility Management</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="#">Services</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Armed and Unarmed Forces</a></li>
+                                    <li><a href="scordservices_sec_off.html">Security Officers</a></li>
+                                    <li><a href="#">Bouncers Security Guards</a></li>
+                                    <li><a href="#">Lady Searchers</a></li>
+                                    <li><a href="#">Supervisors</a></li>
+                                    <li><a href="#">Dog Squad</a></li>
+                                    <li><a href="#">Hand Metal Detectors</a></li>
+                                    <li><a href="#">Door Frame Detectors</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="scordsecurityguard.html">Security Guard</a></li>
+                            <li class="dropdown mega-menu-item"><a href="scordtraining.html">Trainings</a></li>
+                            <li class="dropdown mega-menu-item"><a href="scordcontactus.html">Contact Us</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!--end: Navigation-->
+        </div>
+    </div>
+</header>
