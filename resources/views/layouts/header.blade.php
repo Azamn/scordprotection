a962ab620c30e0664a69fdccfb81249a9bb70926
cshoaib05 <choudharyshoaib05@gmail.com>
2021-09-10 15:09:09 +0530

    Our Services added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,18 +1,24 @@
 <header id="header" class="light header-logo-center">
     <div class="header-inner background-grey">
-        <div class="container"> 
+        <div class="container">
             <div class="header-extras">
                 <ul>
-                   
+
 
                     <li class="mr-3 d-sm-block d-lg-none d-xl-none ">
-                        <a class="btn btn-sm text-white" href="tel:+918689888666"> <i class="icon-phone mr-1"></i> Call Us</a>
+                        <a class="btn btn-sm text-white" href="tel:+918689888666"> <i class="icon-phone mr-1"></i> Call
+                            Us</a>
                     </li>
                     <li class="mr-3">
-                        <a href="https://wa.link/n8903u" target="_blank" style="background-color:#3fe75d; border-style:none" class="btn mr-3  btn-sm text-white"> <i class="fab fa-whatsapp mr-1"></i> Whatsapp</a>
+                        <a href="https://wa.link/n8903u" target="_blank"
+                            style="background-color:#3fe75d; border-style:none" class="btn  btn-sm text-white"> <i
+                                class="fab fa-whatsapp mr-1"></i> Whatsapp</a>
                     </li>
                     <li class="mr-3">
-                        <a class="btn btn-sm text-white" target="_blank" style="background-color:#e34133; border-style:none" href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=support@astuteconsultants.in"><i class="fa fa-envelope mr-1"></i>email</a>
+                        <a class="btn btn-sm text-white" target="_blank"
+                            style="background-color:#e34133; border-style:none"
+                            href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=support@astuteconsultants.in"><i
+                                class="fa fa-envelope mr-1"></i>email</a>
                     </li>
                 </ul>
             </div>
@@ -31,21 +37,21 @@
                             <li class="mr-3"><img src="{{{asset('images/Scord.png')}}}" alt="" width="120px"></li>
                             <li class="mt-3"><a href="#header">Home</a></li>
                             <li class="mt-3"><a href="#aboutus">About Us</a>
-                               
+
                             </li>
                             <li class="mt-3"><a href="#services">Our Services</a>
-                               
+
                             </li>
                             <li class="mt-3"><a href="#contactus">Contact Us</a>
-                               
+
                             </li>
                             <li class="mt-3"><a href="#useful">Our Clients</a>
-                               
+
                             </li>
                         </ul>
 
                         <ul>
-                           
+
                         </ul>
                     </nav>
                 </div>
---
+++ resources/views/main/ourservice.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="body-inner">
+
+    <section id="page-title" data-bg-parallax="{{{asset('images/scordimg/bg.jpg')}}}">
+        <div class="bg-overlay"></div>
+        <div class="container">
+            <div class="page-title">
+                <h1 class="text-uppercase text-medium">OUR SERVICES</h1>
+            </div>
+        </div>
+    </section>
+   
+    <section>
+        <div class="container">
+            <div class="row">
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="heading-text heading-section">
+                            <h2>Our Service</h2>
+
+                        </div>
+
+                        <div>
+                            The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum
+                            gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie
+                            ipsum volutpat quis. A true story, that never
+                            been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, <br> <br> facilisis
+                            ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem
+                            quis lacinia faucibus, orci ipsum gravida
+                            tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum
+                            volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci.
+                            Pellentesque ipsum erat, facilisis ut venenatis
+                            eu, sodales vel dolor.
+                            The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum
+                            gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie
+                            ipsum volutpat quis. A true story, that never
+                            been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, <br> <br> facilisis
+                            ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem
+                            quis lacinia faucibus, orci ipsum gravida
+                            tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum
+                            volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci.
+                            Pellentesque ipsum erat, facilisis ut venenatis
+                            eu, sodales vel dolor.
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 mt-10"><img src="{{{asset('images/scordimg/bg.jpg')}}}" width="100%" alt="Cannot show img"></div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</div>
+
+@endsection
