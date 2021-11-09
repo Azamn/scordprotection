19c4057bc75abd94f8bf42270e9172b7547e7080
cshoaib05 <choudharyshoaib05@gmail.com>
2021-11-09 16:24:55 +0530

    Added

--- resources/views/main/clients.blade.php
+++ resources/views/main/clients.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 
 <section id="page-content" class="p-0">
-    <section id="page-title" data-bg-parallax="{{{asset('images/scordimg/bg.jpg')}}}">
-        <div class="bg-overlay"></div>
+    <section class="background-colored">
+        <div id="particles-dots" class="particles"></div>
         <div class="container">
-            <div class="page-title">
-                <h1 class="text-uppercase text-medium">OUR CLIENTS</h1>
+            <div class="heading-text text-light text-center">
+                <strong>
+                    <h4>OUR CLIENTS</h4>
+                </strong>
+
             </div>
         </div>
     </section>
-    <div class="line"></div>
     <div class="container">
-        <div class="text-center heading-text heading-section">
-            <h2>OUR CLIENTS</h2>
-        </div>
         <div class="row team-members m-t-40">
             <div class="container mt-5 mb-5">
                 <div class="row">
@@ -33,127 +32,6 @@
             </div>
 
             @endforeach
-            {{-- <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/3.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Shabbir Quettawalla</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/4.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Imran Paloba</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/5.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Adv.Mohseen Shaikh</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/6.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Anwar Choudhry</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/7.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Fahim Ahmed kagzi</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/8.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Gulam Chunawala</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/9.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Abdul Rahim Khan</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/10.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Juzer Master</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/11.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Sameer Dabbawala</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/12.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Tabrez Rubberwala</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/13.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Abbas Dohadwala(exp.)</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <div class="team-image card">
-                        <img src="{{asset('images/scordimg/14.jpeg')}}" height="500" style="object-fit: contain; object-position: center;">
-                    </div>
-                    <div class="team-desc">
-                        <h3>Saifee Dalal</h3>
-                    </div>
-                </div>
-            </div> -- --}}
-
         </div>
     </div>
 </section> <!-- end: Content -->
